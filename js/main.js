9557790d08a3da4cea07e29b0cfc7fb847254bf9
Patrick Logan <JPatrickLogan@gmail.com>
2017-03-11 20:06:57 -0700

    feat (index) Add chooseWord drawUnderscore alterAt replace fns

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
+// "Would you like to play?" button
+
+
+
+
+
+
 //code for selecting word from
     function chooseWord () {
     let chooseWord =  words[((Math.floor(Math.random()) * words.length)-1)];
 }
  // RETURN chooseWord()
-let wordLength = chooseWord.length;
 
-function blanksForAnswerWindow ( answerWord ) {
+ let wordLength = chooseWord.length;
+
+
+
+
+
+function drawUnderscores ( chooseWord ) {
 
 	    var result = "";
-	    for (var i = 0; i < answerWord.length; i++) {
+	    for (var i = 0; i < chooseWord.length; i++) {
 	    result = result += "_ ";
 	  }
-	  return result;
+	  return result;  // to the HTML window
 	}
   //  RETURN blanksForAnswerWindow
+
+
 
   function alterAt ( n, c, originalString ) {
       return originalString.substr(0,n) + c + originalString.substr(n+1);}
@@ -21,3 +35,9 @@ function blanksForAnswerWindow ( answerWord ) {
   function alterAt ( 3, l, "helko world")
 
   // replaces correctly-guessed letter
+
+
+  function show (letter, shown, checkLetter) {
+    let s = shown.substr(0,checkLetter) + letter + shown.substr(checkLetter+1, shown.length)
+    return s;
+  }
