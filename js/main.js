c1a7718135246d73268fc60748c7fd73b12f18a3
Patrick Logan <JPatrickLogan@gmail.com>
2017-03-12 17:14:14 -0600

    feat (.js) Display alphabet array on html (querySelector)

--- js/main.js
+++ js/main.js
@@ -12,10 +12,26 @@ var commonWords = [
   "come","made","may","part"
 ];
 
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+let alphNode = document.querySelector("div#alphabet")
+alphNode.textContent = alphabet.join(" ")
 // "Would you like to play?" button
 
-
+// create alphabet ul
+  // var buttons = function () {
+  //   myButtons = document.getElementById('buttons');
+  //   letters = document.createElement('ul');
+  //
+  //   for (var i = 0; i < alphabet.length; i++) {
+  //     letters.id = 'alphabet';
+  //     list = document.createElement('li');
+  //     list.id = 'letter';
+  //     list.innerHTML = alphabet[i];
+  //     check();
+  //     myButtons.appendChild(letters);
+  //     letters.appendChild(list);
+  //   }
 
 
 
