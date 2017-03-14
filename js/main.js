c975240bfec74c1a8ba369e1fa2ac8305fe22ef8
Patrick Logan <JPatrickLogan@gmail.com>
2017-03-13 23:21:58 -0600

    feat (index, bodyparts) Recover alphabet and underscore; add hang-body

--- js/main.js
+++ js/main.js
@@ -1,51 +1,92 @@
 // document.getElementById("Start").onclick = function() {chooseWord()};
-var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
-var commonWords = [
-  "the","cat","and","umbrella","stop","inside","island","you","that","hit","heresy","was","for","pontoon","hare","wash","with","monkey","they","might","clock","beehive","this","have","from","port","tone","handle","butterfly","word","buttress","not","what","all","were","well","when","your","can","said","there","fuse","cannon","each","which","shell","dorry","chow","their","cliff","will","puppy","other","about","out","many","then","them","these","softball","some","hurt","would","make","like","him","into","time","hash","look","two","more","write","gone","see","number","knock","weigh","could","people","harmony","than","first","water","been","call","who","oil","hits","now","find","long","down","daylight","did","forget","come","made","may","part"]
-
+let render = {
+    randomWord: " ",
+    guess: " ",
+    tracker: [],
+    feedback: " ",
+    guessedLetters: [],
+    guessesRemaining: 10,
+    mistakeMessage: " ",
+    head: "hidden",
+    neck: "hidden",
+    torso: "hidden",
+    arm1: "hidden",
+    arm2: "hidden",
+    leg1: "hidden",
+    leg2: "hidden",
+    hang: "hidden",
+    btn: "hidden",
+}
 
 document.getElementById("pick-letter").onclick = function() {pickLetter()};
+
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+var commonWords = [
+  "them","cough","hand","actor","torque","finish","fish","youth","that","bait","help",
+  "wash","fort","only","garish","waste","with","history","they","island","batch","bet",
+  "thank","have","from","order","bone","had","butterfly","word","bunt","notch",
+  "what","ball","were","wet","when","your","scan","said","there",
+  "abuse","nanny","each","which","she","doe","how","their","cliff","will",
+  "puppy","other","about","out","many","then","them","these","son",
+  "some","hurt","would","make","like","him","into","time","hash",
+  "look","two","more","write","gonzo","see","number","note","way",
+  "could","people","max","than","first","water","been","call",
+  "who","oil","its","now","find","long","down","day","did","get",
+  "come","made","may","part"]
+
 
 let alphNode = document.querySelector("div#alphabet")
 
 alphNode.textContent = alphabet.join(" ")
 
+var chooseLetter=document.getElementById("chooseLetter");
+// chooseLetter.addEventListener("input", display);
+
+
 function pickLetter (){
-  var letter = "Your choice of letter?"
-  console.log(letter);
+var letter = prompt("Your choice of letter?");
+console.log(letter);
 }
 
 function chooseWord() {
   let thing = commonWords[(Math.floor(Math.random() * commonWords.length))];
   return thing;
-}
+ }
 
 var word = chooseWord()
+console.log(word)
 
-function drawUnderscores (slippery ) {
+
+function drawUnderscores (word) {
   var result = "";
-  for (var i = 0; i < slippery.length; i++) {
+  for (var i = 0; i < word.length; i++) {
     result += "_ ";
 	}
 	return result;
+  console.log(result);
 }
 
 var underscores = drawUnderscores(word)
 
+function alterAt ( n, c, originalString ) {
+  return originalString.substr(0,n) + c + originalString.substr(n+1);
+}
+
 function guessLetter (letter, shown, answer) {
-  var index=0;
-  index = answer.indexOf(letter);
-  while (index >= 0){
-    shown = alterAt(index, letter, shown);
-    index = answer.indexOf(letter, index + 1) // this makes certain to recognize a second "t" in butter
+  var checkIndex=0;
+  checkIndex = answer.indexOf(letter);
+  while (checkIndex >= 0){
+    shown = alterAt (checkIndex, letter, shown);
+    checkIndex = answer.indexOf(letter, checkIndex + 1)
   }
   return shown;
 }
 
-function alterAt(index, letter, originalString ) {
-  return originalString.substr(0,index) + letter + originalString.substr(index+1);
+  // replaces correctly-guessed letter
+function show (letter, shown, checkLetter) {
+  let s = shown.substr(0,checkLetter) + letter + shown.substr(checkLetter+1, shown.length)
+  return s;
 }
-
 
 document.getElementById("game-board").innerHTML = underscores;
