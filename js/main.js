0eff555399b2c1fc9eb6ab2e8b833ba89adee5c6
captrat <jpatricklogan@gmail.comgit config user.name>
2017-05-30 13:42:44 -0600

    Add letter onclick

--- js/main.js
+++ js/main.js
@@ -1,109 +1,74 @@
-//tries don't count down
-// alphabet wrong guesses isn't working
-// css needs help
-
-
 const hangman = (function() {
 
   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
   var commonWords = [
-    "them","cough","hand","actor","torque","finish","fish","youth","that","bait","help",
-    "wash","fort","only","garish","waste","with","history","they","island","batch","bet",
-    "thank","have","from","order","bone","had","butterfly","word","bunt","notch",
-    "what","ball","were","wet","when","your","scan","said","there",
-    "abuse","nanny","each","which","she","doe","how","their","cliff","will",
-    "puppy","other","about","out","many","then","them","these","son",
-    "some","hurt","would","make","like","him","into","time","hash",
-    "look","two","more","write","gonzo","see","number","note","way",
-    "could","people","max","than","first","water","been","call",
-    "who","oil","its","now","find","long","down","day","did","get",
-    "come","made","may","part"]
+    "them","cough","hand","actor","torque","finish","fish","youth","that","bait","help","wash","fort","only","garish","waste","with","history","they","island","batch","bet","thank","have","from","order","bone","had","butterfly","word","bunt","notch","what","ball","were","wet","when","your","scan","said","there","abuse","nanny","each","which","she","doe","how","their","cliff","will","puppy","other","about","out","many","then","them","these","son","some","hurt","would","make","like","him","into","time","hash","look","two","more","write","gonzo","see","number","note","way","could","people","max","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"]
 
-  let alphNode = document.querySelector("#alphabet")
+  let letter = "";
+
+  let alphNode = document.querySelector("div#alphabet")
+
   alphNode.textContent = alphabet.join(" ")
 
+  let chooseLetter = document.getElementById("letter-input");
 
-  // can modify tries for later difficulty setting
-  var tries = 8;
-  var chooseLetter = '';
-  var guessProgress = '';
+  let btn = document.querySelector("button");
+  btn.addEventListener('click', function(){
+    letter = chooseLetter.value;
+    console.log(letter);
+  })
 
-  var answerWord = chooseWord(commonWords)
-  console.log(answerWord)
 
-  //when user makes guess:
-  function inputGuess(){
-    chooseLetter = document.getElementById("letter-input").value;
-    updateGuess();
-  }
 
-  document.getElementById("pick-letter").onclick = inputGuess;
-
-  //display total tries on page load:
-  document.getElementById("tries").innerHTML = "Tries: " +tries;
-// create Answer word "chooseword"
-  function chooseWord(array) {
-    let thing = array[(Math.floor(Math.random() * array.length))];
+  function chooseWord() {
+    let thing = commonWords[(Math.floor(Math.random() * commonWords.length))];
     return thing;
    }
 
-// create a string of underscores (replacing word length) that shows progress
-  function drawUnderscores(word) {
-    for (var i = 0; i < word.length; i++) {
-      guessProgress += "_ ";
+  let word = chooseWord()
+  console.log(word)
+
+
+  function drawUnderscores (word) {
+    var result = "";
+    for (let i = 0; i < word.length; i++) {
+      result += "_ ";
   	}
-  	return guessProgress;
+  	return result;
+    // console.log(result);
   }
 
-function updateUnderscores(string) {
-  document.getElementById("game-board").innerHTML = string;
-  tries -=1;
-  if (tries === 0) {
-    // return "Sorry.  You lose!"
-    // gameOver();
+  var underscores = drawUnderscores(word)
+
+  function alterAt ( n, c, originalString ) {
+    console.log(n, c, originalString);
+    return originalString.substr(0,n) + c + originalString.substr(n+1);
   }
-}
-  // compare chooseword vs answer word
-function updateGuess() {
-  let ansArr = answerWord.split('');
-  let progArr = guessProgress.split(' ');
-  console.log("ansArr: ", ansArr);
-  console.log("progArr: ", progArr);
-  for (let i = 0; i < ansArr.length; i++) {
-    if (ansArr[i] === chooseLetter) {
-      progArr[i] = chooseLetter;
-    } else {
-      // add letter to "wrong choices"
+
+  function guessLetter (letter, shown, answer) {
+    var checkIndex=0;
+    checkIndex = answer.indexOf(letter);
+    while (checkIndex >= 0){
+      shown = alterAt (checkIndex, letter, shown);
+      checkIndex = answer.indexOf(letter, checkIndex + 1);
+      console.log(letter, shown, answer);
     }
+    return shown;
   }
-  console.log("progArr: ", progArr)
-  guessProgress = progArr.join(' ')
-  console.log(guessProgress);
-  // document.getElementById("game-board").innerHTML = drawUnderscores(guessProgress);
-  updateUnderscores(guessProgress);
-  // updateAlphabet(progArr)
-}
 
-// function updateAlphabet(progArr) {
-//   let alphabetPotential = alphabet.toString();
-//   console.log("alphabet: ", alphabet);
-//   let guessedAlphabet = progArr.split(' ');
-//   console.log("alphabet: ", alphabetPotential);
-//   console.log("progArr: ", progArr);
-//   for (let i = 0; i < alphabet.length; i++) {
-//     if (alphabet[i] === chooseLetter) {
-// add strikethrough to individual "wrong choice letter"
-//  function myFunction() {
-//      document.getElementById("myP").style.textDecoration = "line-through";
-// }
-//       alphabet[i] = chooseLetter;
-//
-// //   document.getElementById("game-board").innerHTML = drawUnderscores(guessProgress);
-// //   updateUnderscores(guessProgress)
-// }
+    // replaces correctly-guessed letter
+  function show (letter, shown, checkLetter) {
+    let s = shown.substr(0,checkLetter) + letter + shown.substr(checkLetter+1, shown.length)
+    return s;
+  }
 
+  document.getElementById("game-board").innerHTML = underscores;
 
-
-  document.getElementById("game-board").innerHTML = drawUnderscores(answerWord);
-})();
+  return {
+    chooseWord: chooseWord,
+    drawUnderscores: drawUnderscores,
+    alterAt: alterAt,
+    guessLetter: guessLetter,
+  }
+})()
